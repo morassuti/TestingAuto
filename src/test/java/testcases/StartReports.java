1e6a20fb61d72e4fe6cdceac44f61a43a4ab0d0c
flavio_morassuti <flavio_morassuti@epam.com>
2023-04-04 12:54:07 +0200

    testng.xml, soft assert, listener, reports and simple test cases

--- src/test/java/testcases/StartReports.java
+++ src/test/java/testcases/StartReports.java
@@ -3,33 +3,30 @@ package testcases;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import reports.ExtentManager;
 
-
-public class LoginTest {
+public class StartReports {
     ExtentTest test;
     ExtentReports report;
 
     @BeforeMethod
     public void init(){
         report = ExtentManager.getReport();
-        test = report.createTest("Login Testing");
+        test = report.createTest("Validate Testing");
+    }
+
+    @AfterMethod
+    public void quit() {
+        report.flush();
     }
 
     @Test
-    public void login(){
-        test.log(Status.INFO, "First Test Case");
+    public void doReports(){
+        test.log(Status.INFO, "Second Test Case");
         test.log(Status.PASS, "Test Passed");
-        //test.addScreenCaptureFromPath("","");
-
-
-
-
-
-
-
 
     }
 }
